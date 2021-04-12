f89e8b3d6f981b1147454e6403686aeebf4fd1c0
shivam6522 <43088585+shivam6522@users.noreply.github.com>
2021-04-12 12:14:56 +0530

    updated

--- README.md
+++ README.md
@@ -3,13 +3,16 @@
 
 ## modify your mongo db connection url as per your database name in [server.js](server.js)
 
-## create a .env file in main folder and set the following values
+## create a ".env" file in the folder which contains "server.js" file and set the following values
 * SESSION_SECRET=
 * GMAIL_ID=
 * CLIENT_ID=
 * CLIENT_SECRET=
 * REFRESH_TOKEN=
 
-* use this video to set up your client id, secret and refresh token https://www.youtube.com/watch?v=-rcRf7yswfM
+* use this video to set up your client id, client secret and refresh token https://www.youtube.com/watch?v=-rcRf7yswfM
+* keep any string as your session secret (longer and complex session secret is preferred)
+
+## And now you can run the application just by the command "npm run start" !!
 
 ## reset password functionality yet to be added....
--- server.test.js
+++ server.test.js
@@ -1,1 +1,111 @@
-const {}
+/********require('jest')*******/
+const puppeteer = require('puppeteer')
+
+/*
+test ('should fail to login with invalid/unregistered email', async (done) =>{
+    jest.setTimeout(20000);
+    const browser =  await puppeteer.launch({
+        headless: false,
+        slowMo: 20, //increase it to slow down
+        args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/login');
+    await page.click('input#email');
+    await page.type('input#email','shiv6522@gmail.com');
+    await page.click('input#password');
+    await page.type('input#password','88767777');
+    await page.click('button#signin');
+    done();
+});
+
+test ('should fail to login with wrong password', async (done) =>{
+    jest.setTimeout(20000);
+    const browser =  await puppeteer.launch({
+        headless: false,
+        slowMo: 20, //increase it to slow down
+        args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/login');
+    await page.click('input#email');
+    await page.type('input#email','shivam6522@gmail.com');
+    await page.click('input#password');
+    await page.type('input#password','88767777');
+    await page.click('button#signin');
+    done();
+});
+*/
+
+// end-to-end testing
+test ('should fail to access home page after logging out', async (done) =>{
+    jest.setTimeout(20000);
+    const browser =  await puppeteer.launch({
+        headless: false,
+        slowMo: 20, //increase it to slow down
+        args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/login');
+    await page.click('input#email');
+    await page.type('input#email','shivam6522@gmail.com');
+    await page.click('input#password');
+    await page.type('input#password','88887777');
+    await page.click('button#signin');
+    await page.goto('http://localhost:3000/');
+    await page.click('button#logout');
+    await page.goto('http://localhost:3000/');
+    done();
+});
+
+test ('should be able to access home page after successful login', async (done) =>{
+    jest.setTimeout(20000);
+    const browser =  await puppeteer.launch({
+        headless: false,
+        slowMo: 20, //increase it to slow down
+        args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/login');
+    await page.click('input#email');
+    await page.type('input#email','shivam6522@gmail.com');
+    await page.click('input#password');
+    await page.type('input#password','88887777');
+    await page.click('button#signin');
+    await page.goto('http://localhost:3000/');
+    done();
+});
+
+/*
+test ('should login and change password successfully', async (done) =>{
+    jest.setTimeout(20000);
+    const browser =  await puppeteer.launch({
+        headless: false,
+        slowMo: 20, //increase it to slow down
+        args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    // to login first
+    await page.goto('http://localhost:3000/login');
+    await page.click('input#email');
+    await page.type('input#email','shivam6522@gmail.com');
+    await page.click('input#password');
+    await page.type('input#password','88887777');
+    await page.click('button#signin');
+    await page.goto('http://localhost:3000/');
+
+    // to change password
+    await page.click('button#changePassword');
+    await page.goto('http://localhost:3000/change_password');
+    await page.click('input#email');
+    await page.type('input#email', 'shivam6522@gmail.com');
+    await page.click('input#password');
+    await page.type('input#password','88887777');
+    await page.click('input#new_password');
+    await page.type('input#new_password', '77776666');
+    await page.click('input#confirm_password');
+    await page.type('input#confirm_password', '77776666');
+    await page.click('button#submit');
+    done();
+});
+*/
